2d1cc9e6421255a459e3d72ce28c401641f08661
Sandrine Morard <sandrinemorard@gmail.com>
2014-07-09 13:03:18 +0200

    Exo 5_coach_du 8 juillet pas fini

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,15 @@
-def coach_answer(your_message)
+def coach_answer(sentence)
   # TODO: return coach answer to your_message
+  if sentence.include?('?')
+    "Silly question, get dressed and go to work !"
+  elsif sentence == "I am going to work right now SIR !"
+    ""
+  else
+    "I don't care son, get dressed and go to work !"
 end
 
-def coach_answer_enhanced(your_message)
+def coach_answer_enhanced(sentence)
   # TODO: return coach answer to your_message, with additional custom rules of yours !
+  if sentence.include?('!') puts "I can feel your motivation."
 end
 
