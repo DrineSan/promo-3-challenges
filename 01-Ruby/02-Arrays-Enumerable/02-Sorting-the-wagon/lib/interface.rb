2ef9b3e540748150986d07acabbb3f5999fc15db
Sandrine Morard <sandrinemorard@gmail.com>
2014-07-09 13:59:23 +0200

    Exo 2_sorting_du 9 juillet pas réussi

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -5,10 +5,13 @@ students = []
 begin
   puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
   name = gets
-  name.chomp! if name
-
+  name.chomp! if name != " "
+  students << name
   # TODO: Add the name we just gotto the students array
 
 end while name != ""
 
 # TODO: Call `wagon_sort` method and display the sorted student list
+wagon_sort(students)
+print students
+puts students.count
