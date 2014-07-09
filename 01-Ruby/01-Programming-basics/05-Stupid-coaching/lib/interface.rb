c5fa78081ffb172fcfbcf5aad9c38020f5a3df9f
Sandrine Morard <sandrinemorard@gmail.com>
2014-07-09 13:57:42 +0200

    Exo 5_coaching_du 8 juillet pas réussi

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,13 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
 
+puts "Poser une question au coach."
+answer = gest.chomp
+
+until answer == "I am going to work right now SIR !"
+  puts coach_answer(sentence)
+  puts 'Poser une question au coach'
+  answer = gets.chomp
+end
+
+
