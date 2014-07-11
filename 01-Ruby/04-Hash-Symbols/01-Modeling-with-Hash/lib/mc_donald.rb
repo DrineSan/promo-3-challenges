81b1368c10acbe9f21bbad9b8318224ca2d0a596
Sandrine Morard <sandrinemorard@gmail.com>
2014-07-11 11:48:39 +0200

    Exo 1_hash_pas complètement réussi

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,7 +1,27 @@
 def poor_calories_counter(burger, side, beverage)
   #TODO: return number of calories for this mcDonald order
+  burgers = {
+   "Cheese Burger" => 290,
+   "Big Mac"=> 300,
+   "Mc Bacon"=> 400,
+   "Royal Cheese"=> 130
+  }
+
+  sides = {
+    "French fries"=> 130,
+    "Potatoes" => 130
+  }
+
+  beverages = {
+    "Coca" => 160,
+    "Sprite" => 170
+  }
+
+  burgers[burger] + sides[side] + beverages[beverage]
+
 end
 
 def calories_counter(*orders)
   #TODO: return number of calories for a less constrained order
+  #orders[order]
 end
