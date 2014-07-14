a82893784c294aec7f4c3067f3a9dc69d0c81165
Sandrine Morard <sandrinemorard@gmail.com>
2014-07-14 15:15:57 +0200

    en cours

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -3,4 +3,13 @@ require 'csv'
 
 def most_successfull(number, max_year, file_name)
   #TODO: return the number most successfull movies max_year
+
+  csv_options = { col_sep: ',', force_quotes: true, quote_char: '"' }
+  filepath    = 'code/DrineSan/promo-3-challenges/01-Ruby/06-Parsing/01-CSV-Parsing/movies.csv'
+
+  CSV.foreach(filepath, csv_options) do |row|
+    CSV.select { |num|  num < max_year }
+    puts "#{row['Name']}, #{row['Year']}, #{row['Box_office']}"
+  end
+
 end
