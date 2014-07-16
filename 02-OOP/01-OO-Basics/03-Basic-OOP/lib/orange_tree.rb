63e0b82891e1d45a6a77dec851b2ee396dda2948
Sandrine Morard <sandrinemorard@gmail.com>
2014-07-16 16:28:51 +0200

    Exo 3_OrangeTree presque réussi - pb avec pick an orange

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,50 @@
 class OrangeTree
   # TODO: Implement all the specs defined in the README.md :)
+
+attr_reader :age, :fruits, :height
+
+  def initialize
+    @age = 0
+    @height = 0
+    @fruits = 0
+  end
+
+  def dead?
+    if @age <= 50
+      false
+    elsif @age >= 100
+      true
+    else
+      [true, false].sample
+    end
+  end
+
+
+  def one_year_passes!
+    @age += 1
+    if @age <= 5
+      @fruits =0
+    elsif @age > 15
+      @fruits = 0
+    elsif @age <= 10
+      @fruits = 100
+    else
+      @fruits = 200
+    end
+    if @age <= 10
+      @height = @age
+    else
+      @height = 10
+    end
+  end
+
+  def pick_a_fruit!
+    for @fruits in (1..200)
+      @fruits -= 1
+    end
+  end
+
 end
+
+
+
