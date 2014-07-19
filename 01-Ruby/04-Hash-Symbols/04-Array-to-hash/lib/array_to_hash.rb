725b7a9156cbdc6ede7a72759f72c283ea5a97e8
Sandrine Morard <sandrinemorard@gmail.com>
2014-07-19 22:06:25 +0200

    Exo_array_to_hash_ done

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,11 +1,34 @@
 def array_to_hash(array)
-  #TODO: implement the method :)
-hash = {}
 
-array.each_with_index do |value, index|
-  hash[index] = value
-end
-hash
+  hash = {}
+
+  # method array_to_hash called without a block
+  # ex:
+  #   array_to_hash(["a", "b", "c"])
+  if !block_given?
+
+    array.each_with_index do |value, index|
+      hash[index] = value
+    end
+
+  # method array_to_hash called with a block
+  # ex:
+  #   array_to_hash(["a", "b", "c"]) { |index| "key#{index + 1}" }
+  # ex2:
+  #   array_to_hash(["a", "b", "c"]) do |index|
+  #     "key#{index + 1}"
+  #   end
+  else
+
+    array.each_with_index do |value, index|
+      key = yield(index)
+      hash[key] = value
+    end
+
+  end
+
+  hash
+
 end
 
-puts array_to_hash([a,b,c,d])
+puts array_to_hash(["a", "b", "c"])
