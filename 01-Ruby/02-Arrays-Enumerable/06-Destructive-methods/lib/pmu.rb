f919ea1ed8162a434b21e1d04e89de59f6cb9cda
Sandrine Morard <sandrinemorard@gmail.com>
2014-07-19 13:41:16 +0200

    Exo 6_destrictive methods done

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,5 +1,10 @@
 def pmu_format!(race_array)
   #TODO: modify the given array so that it is PMU-consistent
+  race_array.each_with_index do |horse, index|
+    race_array[index] = "#{index+1}-#{horse}!"
+  end
+  race_array.reverse!
+  race_array
 end
 
 
