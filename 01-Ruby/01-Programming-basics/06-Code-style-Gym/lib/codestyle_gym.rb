04feee829bafb3e45c786c1ae057e1bf3e69cc99
Sandrine Morard <sandrinemorard@gmail.com>
2014-07-19 13:02:12 +0200

    Exo 6_coding style done

--- 01-Ruby/01-Programming-basics/06-Code-style-Gym/lib/codestyle_gym.rb
+++ 01-Ruby/01-Programming-basics/06-Code-style-Gym/lib/codestyle_gym.rb
@@ -1,48 +1,54 @@
-class Player_moving
-  grid_positions = (1..100).to_a
+class PlayerMoving
+  GRID_POSITIONS = (1..100).to_a
 
-def initialize(position)
-  position = grid_positions[0];
-  raise "Initial position must be between 1 and 100" unless grid_positions.include?(position)
-end
+  def initialize(position)
+    @position = position
+    @iterations = 0
+    raise "Initial position must be between 1 and 100" unless GRID_POSITIONS.include? @position
+  end
 
-def move(distance_in_meters)
-  position += distance_in_meters
-end
+  def move(distance_in_meters)
+    @position += distance_in_meters
+  end
 
-def play
-  if self_status_player == play
-  && rand(1..100) < 50 move(rand(1..10))
-  else
+  def play
+    if self.status_player == :play
+      if rand(1..100) < 50
+        move(rand(1..10))
+      else
         move(-rand(1..10))
-  end
-  end
-end
-
-def cheat(position)
-  position[101]
-end
-
-def status_player
-    if position > 100 puts "win"
-    elsif position < 0) puts "loose"
-    else puts "play"
-end
-
-def has_won
-  status_player  == win
+      end
+    end
   end
 
+  def cheat
+    @position = 101
+  end
+
+  def status_player
+    if @position > 100
+      :win
+    elsif @position < 0
+      :loose
+    else
+      :play
+    end
+  end
+
+  def has_won
+    status_player  == :win
+  end
+
 end
 
-player = Player_moving.new(50)
+player = PlayerMoving.new(50)
 player.play
-puts player.status_Player == :play # true
+puts player.status_player == :play # true
 player.cheat
-puts player.hasWon == true
+puts player.has_won == true
 
 begin
-  Player_moving.new(110)
+  PlayerMoving.new(110)
 rescue RuntimeError => e
   puts e.to_s == "Initial position must be between 1 and 100" # true
 end
