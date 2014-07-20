866b9c2c0be5493fb16e9e4e14f8701c78f95312
Sandrine Morard <sandrinemorard@gmail.com>
2014-07-20 10:12:25 +0200

    Exo_roman to integer_ done

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,5 +1,30 @@
 def roman_to_integer(roman_string)
   #TODO: translate roman string to integer
+  single_characters = {
+    "I" => 1,
+    "V" => 5,
+    "X" => 10,
+    "L" => 50,
+    "C" => 100,
+  }
+  roman_letters = roman_string.split(//)
+
+  integers = roman_letters.map { |letter| single_characters[letter] }
+
+  result = 0
+
+  integers.each_with_index do |integer, n|
+    if integers[n+1] == nil
+      result += integer
+    elsif integer < integers[n+1]
+      result -= integer
+    else
+      result += integer
+    end
+  end
+
+  result
+
 end
 
 # testing your program
