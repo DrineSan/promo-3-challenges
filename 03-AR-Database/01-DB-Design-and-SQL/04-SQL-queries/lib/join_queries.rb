6b570b471fd58a52fc52665da5ddf4118ed3f6f9
Sandrine Morard <sandrinemorard@gmail.com>
2014-07-28 11:09:17 +0200

    Exo à finir

--- 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
@@ -4,12 +4,17 @@ require 'sqlite3'
 DATABASE_PATH = "db/jukebox.sqlite"
 db = SQLite3::Database.new(DATABASE_PATH)
 
+# first_result = rows[0]
+# first_result[0]
+
 def detailed_tracks(db)
   # TODO: return the list of tracks with their album and artist
+  db.execute "SELECT t.name, a.title, ar.name FROM Track t, Album a, Artist ar"
 end
 
 def stats_on(db, category)
   #TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
+
 end
 
 def top_five_rock_artists(db)
--- 03-AR-Database/02-SQL-CRUD/lib/crud.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/crud.rb
@@ -2,13 +2,21 @@ require 'sqlite3'
 
 def create_scheme(db)
   #TODO: create your db scheme
-  sql = %q{ 
-  }  # %q allows for multiline string in ruby 
+  sql = %q{ CREATE TABLE news
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(100)
+    source_url VARCHAR(200)
+    date DATETIME,
+    RATING INT
+      )
+  }  # %q allows for multiline string in ruby
   db.execute(sql)
 end
 
 def create_post(db, post)
   #TODO: add a new post to your db
+  INSERT INTO db
+  VALUES post
 end
 
 def get_posts(db)
