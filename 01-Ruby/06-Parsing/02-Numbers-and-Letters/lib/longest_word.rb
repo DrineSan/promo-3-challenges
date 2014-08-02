fe255b8dfb0f98b22682c03421aa7d76178c97ed
Sandrine Morard <sandrinemorard@gmail.com>
2014-08-02 13:38:02 +0200

    WIP Exo Parsing longest word

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -1,10 +1,19 @@
 require 'open-uri'
 require 'json'
 
+# attr_reader :grid
+
 def generate_grid(grid_size)
   #TODO: generate random grid of letters
+  grid = []
+  grid_size.times { grid << ('A'..'Z').to_a.sample}
+return grid
 end
+
 
 def run_game(attempt, grid, start_time, end_time)
   #TODO: runs the game and return detailed hash of result
+  json_url = "http://api.wordreference.com/0.8/80143/json/enfr/apple"
+  hash = {}
+
 end
