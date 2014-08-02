bbc5daaf786f4db8dbba5550ddd0b7aa07f5c4e4
Sandrine Morard <sandrinemorard@gmail.com>
2014-08-02 17:28:45 +0200

    Exo regexp 1 done

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,14 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  phone_number = phone_number.delete(" -")
+  if phone_number.match(/\A(?:0|\+3{2})[1-9]\d{8}\z/)
+    true
+  else
+    false
+  end
+
+  # autre syntaxe:
+  #   !!phone_number.match(/\A[0\d|3{2}]\d{8}\z/)
 end
+
+p french_phone_number?('0134257699')
