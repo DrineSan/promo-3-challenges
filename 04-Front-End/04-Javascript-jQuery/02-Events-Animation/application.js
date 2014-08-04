dd18dc83a75e5f2d5d84183a63f9b69ac20d8216
Sandrine Morard <sandrinemorard@gmail.com>
2014-08-04 15:41:20 +0200

    Exo events animation done

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,11 @@
   * TODO: add your code here!
   *
 */
+
+$( "#message-wrapper" ).load(function() {
+  $( "#message-wrapper" ).slideDown(slow);
+});
+
+$( "#message-wrapper" ).click(function() {
+  $( "#message-wrapper" ).slideUp(5000);
+});
