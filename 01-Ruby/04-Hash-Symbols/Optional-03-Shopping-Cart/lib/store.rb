9c7915afb9dfac3a65d3e931e7690714b2c11c3b
Sandrine Morard <sandrinemorard@gmail.com>
2014-08-07 16:03:15 +0200

    Exo done, deux tests sur 9 ne passent pas

--- 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/store.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/store.rb
@@ -2,26 +2,30 @@
 # You should complete it according to the instructions in the TODO comments
 
 def store_items
-  # Here's what you should have in store:
-  #   yogurts : 2€
-  #   meat : 7€
-  #   vegetables: 5€
-  #   potatoes : 2€
-  #   rice : 1€
-
-  # TODO: find the correct data structure to hold the shopping store items
-  # instead of using nil below
-  store = nil
-
+store = {
+    "yogurts" => 2,
+    "meat" => 7,
+    "vegetables" => 5,
+    "potatoes" => 2,
+    "rice" => 1
+  }
   return store
 end
 
 def is_product_in_store?(product)
   # TODO: Check whether product is present in the store items
+  products = store_items
+  products.has_key?(product)
 end
 
 def price_of_product(product)
   # TODO: Return the price of the given product, if it's in store. nil otherwise
+  if is_product_in_store?
+    price = store[product]
+  else
+    price = nil
+  end
+  return price
 end
 
 def store_items_to_s
@@ -31,4 +35,9 @@ def store_items_to_s
   # ...
   #
   # TIP: add line breaks in your String with "\n"
+  string = ""
+  store_items.each do |product, price|
+    string += "- #{product.capitalize} : #{price}€\n"
+  end
+  return string
 end
